d7a01028ac1400173fefc4f92226123abab3d5dd
acatic2 <acatic2@etf.unsa.ba>
2019-10-21 09:30:45 +0200

    Dodane metode

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,43 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private int kapacitet=50;
+    private int velicina=0;
+    private Artikl[] artikli=new Artikl[kapacitet];
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Korpa(){
+        velicina=0;
+        artikli=new Artikl[kapacitet];
+    }
+    public int dajUkupnuCijenuArtikala(){
+        int suma=0;
+        for(int i=0;i<velicina;i++) suma+=this.artikli[i].getCijena();
+        return suma;
+    }
+    public boolean dodajArtikl(Artikl a){
+        if(velicina>=kapacitet) return  false;
+        this.artikli[velicina]=new Artikl(a.getNaziv(),a.getCijena(),a.getKod());
+        velicina++;
+        return  true;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod){
+        Artikl artiklZaIzbaciti=new Artikl();
+        for(int i=0; i<velicina; i++) {
+            if(this.artikli[i].getKod().equals(kod)) {
+                artiklZaIzbaciti=this.artikli[i];
+                this.artikli[i] = null;
+                for(int j=i;j<velicina-1;j++) {
+                    this.artikli[j]=this.artikli[j+1];
+                }
+                this.artikli[velicina-1]=null;
+                velicina--;
+                break;
+            }
+        }
+        return artiklZaIzbaciti;
+    }
 }
