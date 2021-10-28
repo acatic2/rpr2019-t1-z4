b6d245e0368891ff99ca48b4088307b56a13bfeb
acatic2 <acatic2@etf.unsa.ba>
2021-10-28 13:57:59 +0200

    neke prepravke

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -2,31 +2,32 @@ package ba.unsa.etf.rpr;
 
 public class Supermarket {
     private int kapacitet=1000;
-    private int velicina=0;
+    private int duzina =0;
     public Artikl[] artikli=new Artikl[kapacitet];
 
     public Artikl[] getArtikli() {
         return artikli;
     }
     public void dodajArtikl(Artikl a){
-        if(velicina>=kapacitet) return;
-        artikli[velicina++]=new Artikl(a.getNaziv(),a.getCijena(),a.getKod());
+        if(duzina >=kapacitet) return;
+        artikli[duzina++]=new Artikl(a.getNaziv(),a.getCijena(),a.getKod());
     }
     public Supermarket(){
         artikli=new Artikl[kapacitet];
-        velicina=0;
+        duzina =0;
     }
     public Artikl izbaciArtiklSaKodom(String kod){
         Artikl artiklZaIzbaciti=new Artikl();
-        for(int i=0;i<velicina;i++){
+        for(int i = 0; i< duzina; i++){
             if(this.artikli[i].getKod().equals(kod)){
                 artiklZaIzbaciti=this.artikli[i];
                 artikli[i]=null;
-                for(int j=i;j<velicina-1;j++) this.artikli[j]=this.artikli[j+1];
-                artikli[velicina-1]=null;
+                for(int j = i; j< duzina -1; j++) this.artikli[j]=this.artikli[j+1];
+                artikli[duzina -1]=null;
                 break;
             }
-        }velicina--;
+        }
+        duzina--;
         return  artiklZaIzbaciti;
     }
 }
